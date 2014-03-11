0c2203c3bbccd84612111cdc12f0bb193f831068
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-03-11 16:57:11 -0400

    Virgin commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImanPortfolio::Application.config.secret_key_base = '46c60ad03449992dd63ba0cbd19e59bfd65abfb5594c58539a61cbd873f805896d2b8c3644f56719107661fecc153894dad2368abfb2a5b224b685ff7d0b039b'
