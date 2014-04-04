ea6c5654354b6f954b1e5ba6d4367f2be5666796
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-04-03 23:26:59 -0400

    Implement simple log in and log out functionality (with some alert front-end)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,16 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			# user is signed in, redirect to cms dashboard
+			session[:user_id] = user.id
+			redirect_to root_path
 		else
-			flash.now[:error] = "Invalid email/password combination"	
+			@flash = flash.now[:error] = "Invalid email/password combination"	
 			render 'new'
+		end
 	end
 
 	def destroy
+		session[:user_id] = nil
+		redirect_to root_url, notice: "Logged out"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ ImanPortfolio::Application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :destroy]
   resources :sessions, only: [:new, :create, :destroy] 
 
-  get "admin", to: "sessions#new"
+  get "admin", to: 'sessions#new'
+  get "logout", to: 'sessions#destroy'
   get "users/new"
   get "users/create"
   get "users/edit"
