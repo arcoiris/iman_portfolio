39d1002ca0e8812c93c79c4df85bdf66514df626
Deborah <deborah@arcoiris-ThinkPad-T400.(none)>
2014-04-06 18:21:00 -0400

    Add working reset password functionality with a less than perfect front-end

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,13 +14,33 @@ class PasswordResetsController < ApplicationController
   
 	def update
 	  @user = User.find_by_password_reset_token!(params[:id])
+	  safe_password = params.require(:user).permit(:password, :password_confirmation)
+	  # binding.pry
+	  # @user.update safe_password
+	  # flash[:notice] = "password reset"
+	  # if @user.update_attributes(user_params)
+	  # 	flash[:notice] = "Password reset!"
+	  # 	redirect_to root_path
+	  # end
+
 	  if @user.password_reset_sent_at < 2.hours.ago
-	    redirect_to new_password_reset_path, :alert => "Password reset has expired."
-	  elsif @user.update_attributes(params[:user])
-	    redirect_to root_url, :notice => "Password has been reset!"
-	  else
-	    render :edit
+	  	redirect_to new_password_reset_path, alert: "Password reset token is expired. Send a new email."
+	  	redirect_to root_path, notice: "You are not authorized to perform this action."
+	  elsif @user.update safe_password
+	  	flash[:notice] = "Your password has now been reset!"
+	  	redirect_to admin_path
+	  else 
+	  	render :edit 
 	  end
+
+
+	  # if @user.password_reset_sent_at < 2.hours.ago
+	  #   redirect_to new_password_reset_path, :alert => "Password reset has expired."
+	  # elsif 
+	  #   redirect_to root_url, :notice => "Password has been reset!"
+	  # else
+	  #   render :edit
+	  # end
 	end
 end
 
