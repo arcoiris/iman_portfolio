ee805b8600face243dbb188d90410ef99507d7fd
arcoiris <deborlora@gmail.com>
2014-05-31 15:15:07 -0400

    Add directive to precompile flexslider elements

--- config/application.rb
+++ config/application.rb
@@ -21,4 +21,6 @@ module ImanPortfolio
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+
+  config.assets.precompile += ["assets/fonts*"]
 end
