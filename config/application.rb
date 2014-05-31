31ca8c5018b034e8200d6f817f13a2c19b1a10cd
arcoiris <deborlora@gmail.com>
2014-05-31 15:16:50 -0400

    Fix error in asset precompilation directive syntax

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module ImanPortfolio
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
 
-  config.assets.precompile += ["assets/fonts*"]
+    config.assets.precompile += ["assets/fonts*"]
+  end
 end
