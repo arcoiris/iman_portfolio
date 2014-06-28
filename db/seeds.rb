9b2cdccf7dfc0e1a3d0160665006a968c9e18fef
arcoiris <deborlora@gmail.com>
2014-06-28 19:01:00 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,27 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 StaticContent.create(about: "
-man Jazmin Brown, a native of Southern California was 
-ignited into her purpose at the age of 14 as she booked 
-her first modeling gig at a community event in Los Angeles. 
-That initial flame has evolved Iman into one of the most 
-sought out models for acclaimed designers as well as up 
-and coming trendsetters in the fashion world. Iman 
-has graced numerous runways in the LosAngeles and 
-San Diego regions which include: L.A. Fashion Week, 
-Woodbury College, Charity Event sponsored by Prince Harry 
-and the list goes on and on. Iman's professional status has
- catapultedher into the New York high fashion arena. 
- Now a resident of New York City, Iman has quickly stirred 
- an evolution in the modeling world as she exudes an
-  undeniable confidence and inner beauty. Iman participated 
-  in New York Fashion Week, Brooklyn Fashion Week, Couture 
-  Fashion Week, Vienne Hu on-linecatalog, print and a vast 
-  of photo shoots from photographers who continue to request 
-  Iman's elitesimage for their portfolios. 
-  Behold \"The New Generation Iman\", full of faith, 
-  confidence, innate beauty and elegance.", contact_email: "loveimanb@yahoo.com", contact_number: "718-100-8000", contact_blurb: "Contact me")
+Iman Jazmin Brown, a native of Southern California 
+was ignited into her purpose at the age of 14 as she
+ booked her first modeling gig at a community event 
+ in Los Angeles. That initial flame has evolved Iman
+  into one of the most sought out models for acclaimed 
+  designers as well as up and coming trendsetters in 
+  the fashion world. Iman has graced numerous runways
+   in the Los Angeles and San Diego regions which include: 
+   L.A. Fashion Week, Woodbury College, and a charity 
+   event sponsored by Prince Harry and the list goes on 
+   and on. Iman's professional status has catapulted her
+    into the New York high fashion arena. Now a resident
+     of New York City, Iman has quickly stirred an 
+     evolution in the modeling world as she exudes an
+      undeniable confidence and inner beauty. Iman 
+      participated in New York Fashion Week, Brooklyn 
+      Fashion Week, Couture Fashion Week, Vienne Hu's 
+      online catalog, print and a vast number of photo 
+      shoots from photographers who continue to request
+       Iman's elite images for their portfolios. 
+       Behold "\The New Generation Iman\", full of faith, 
+       confidence, innate beauty and elegance.", contact_email: "loveimanb@yahoo.com", contact_number: "718-100-8000", contact_blurb: "Contact me")
 
 puts "seeded"
