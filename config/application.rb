f02b7c3b2f11ef1b061c450ae2a6505a6be5a94c
arcoiris <deborlora@gmail.com>
2014-08-21 18:47:14 -0400

    Remove precompile calls

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,5 @@ module ImanPortfolio
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    app.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
-    app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
